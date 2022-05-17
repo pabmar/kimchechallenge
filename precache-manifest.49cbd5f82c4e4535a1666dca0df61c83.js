d6abee530d96fc1251e36463ab692da799b9d6a9
Pablo Marchant <pabmar1290@gmail.com>
2022-05-17 15:47:16 -0400

    Updates

---
+++ precache-manifest.49cbd5f82c4e4535a1666dca0df61c83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e49ecdacaaf220f91b9ecb1570dc28",
+    "url": "/kimchechallenge/index.html"
+  },
+  {
+    "revision": "67a5811d0795e6df8084",
+    "url": "/kimchechallenge/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa0029005c4b64f01be2",
+    "url": "/kimchechallenge/static/js/2.2ffa6d01.chunk.js"
+  },
+  {
+    "revision": "61ccd917c66745ba9b5ec183c6f024c0",
+    "url": "/kimchechallenge/static/js/2.2ffa6d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a5811d0795e6df8084",
+    "url": "/kimchechallenge/static/js/main.2766f829.chunk.js"
+  },
+  {
+    "revision": "897be0b3edeac46ce35e",
+    "url": "/kimchechallenge/static/js/runtime-main.a86b64b2.js"
+  }
+]);
